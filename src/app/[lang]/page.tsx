0abebb6249935de701109edd8038639a627f407c
유림 <yulimh001@gmail.com>
2025-12-25 08:25:29 +0900

    feat: 메인 페이지

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,5 +1,14 @@
-export default function Home() {
+import Header from '@/components/common/Header'
+
+interface HomeProps {
+  params: Promise<{ lang: string }>
+}
+
+export default async function Home({ params }: HomeProps) {
+  const { lang } = await params
   return (
-    <main></main>
+    <main>
+      <Header headerType={'default'} currentLng={lang}></Header>
+    </main>
   )
 }
