255f6435a3be494ab8d9aea9ff7169c8695d939a
유림 <yulimh001@gmail.com>
2025-12-25 08:23:44 +0900

    feat: input placeholder추가

--- src/components/common/TextInput.tsx
+++ src/components/common/TextInput.tsx
@@ -26,24 +26,26 @@ export default function TextInput({
   status = 'default',
   buttonElement,
   helperText,
+  placeholder,
 }: TextInputProps) {
-  const textFieldBase = 'border p-4 bg-white rounded-[12px] kr-body-md'
-  const textAreaBase = 'border py-3 px-4 bg-white rounded-[16px] kr-body-md h-[147px]'
+  const textFieldBase = 'border p-4 bg-white rounded-[12px] kr-body-md flex-1 min-w-0 outline-none transition'
+  const textAreaBase =
+    'border py-3 px-4 bg-white rounded-[16px] kr-body-md h-[147px] flex-1 min-w-0 outline-none transition'
   const statusStyle = statusStyleType[status]
   const textFieldClassName = [textFieldBase, statusStyle].join(' ')
   const textAreaClassName = [textAreaBase, statusStyle].join(' ')
 
   return textType === 'textField' ? (
     <div className="flex flex-col gap-y-2">
-      <div className="flex gap-x-2">
-        <input className={textFieldClassName} />
+      <div className="flex w-full gap-x-2">
+        <input placeholder={placeholder} className={textFieldClassName} />
         {buttonElement && buttonElement}
       </div>
       {helperText && <p className="kr-badge-md text-error">{helperText}</p>}
     </div>
   ) : (
-    <div className="flex flex-col gap-y-2">
-      <textarea className={textAreaClassName} />
+    <div className="flex w-full flex-col gap-y-2">
+      <textarea placeholder={placeholder} className={textAreaClassName} />
       {helperText && <p className="kr-badge-md text-error">{helperText}</p>}
     </div>
   )
