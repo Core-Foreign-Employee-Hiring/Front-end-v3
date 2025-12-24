862fcadb8e7413252595762c23df8878a4161638
유림 <yulimh001@gmail.com>
2025-12-25 08:24:46 +0900

    feat: 여백 컴포넌트 기능 구현

---
+++ src/components/common/Spacing.tsx
@@ -0,0 +1,16 @@
+interface SpacingProps {
+  width?: number
+  height?: number
+}
+export default function Spacing({ width = 0, height = 0 }: SpacingProps) {
+  return (
+    <div
+      style={{
+        width: width ? `${width}px` : undefined,
+        height: height ? `${height}px` : undefined,
+        flexShrink: 0, // flex 부모 안에서 줄어들지 않도록 방지
+      }}
+      aria-hidden="true"
+    />
+  )
+}
