e83e9a00bb0b8f55bc7845be483f7e6ecb0c6745
유림 <yulimh001@gmail.com>
2025-12-26 20:25:49 +0900

    feat: dropdown 컴포넌트 기능 구현

---
+++ src/assets/svgComponents/DropDownGray4Icon.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgDropDownGray4Icon = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 20 20" {...props}><mask id="drop-down-gray4-icon_svg__a" width={20} height={20} x={0} y={0} maskUnits="userSpaceOnUse" style={{
+    maskType: "alpha"
+  }}><path fill="#D9D9D9" d="M0 0h20v20H0z" /></mask><g mask="url(#drop-down-gray4-icon_svg__a)"><path fill="#999BA5" d="M9.604 11.604 6.643 8.642a.4.4 0 0 1-.092-.133A.364.364 0 0 1 6.896 8h6.208q.168 0 .272.113a.38.38 0 0 1 .103.262q0 .021-.123.266l-2.96 2.963a.555.555 0 0 1-.792 0" /></g></svg>;
+export default SvgDropDownGray4Icon;
---
+++ src/components/common/DropDown.tsx
@@ -0,0 +1,72 @@
+import { ReactNode } from 'react'
+import { DropDownGray3Icon, DropDownGray4Icon } from '@/assets/svgComponents'
+import { twMerge } from 'tailwind-merge'
+
+interface DropDownProps {
+  isDropDownOpen: boolean
+  dropDownHandler: () => void
+  children: ReactNode
+  defaultValue: string
+  selectedValue: string | undefined
+  className?: string
+}
+
+export default function DropDown({
+  children,
+  isDropDownOpen,
+  dropDownHandler,
+  defaultValue,
+  selectedValue,
+  className,
+}: DropDownProps) {
+  return (
+    <div className={twMerge('relative w-full', className)}>
+      <SelectedDropBoxContentBox
+        dropDownHandler={dropDownHandler}
+        isDropDownOpen={isDropDownOpen}
+        selectedValue={selectedValue}
+        defaultValue={defaultValue}
+      />
+      {isDropDownOpen ? <DropBoxOptionBox>{children}</DropBoxOptionBox> : null}
+    </div>
+  )
+}
+function SelectedDropBoxContentBox({
+  defaultValue,
+  selectedValue,
+  dropDownHandler,
+  isDropDownOpen,
+}: {
+  defaultValue: string
+  selectedValue: string | undefined
+  dropDownHandler: () => void
+  isDropDownOpen: boolean
+}) {
+  return (
+    <div
+      onClick={dropDownHandler}
+      className={`${!selectedValue ? 'text-gray4' : 'text-black'} kr-body-md border-gray2 flex h-[52px] items-center justify-between rounded-[12px] border bg-white px-4 py-3`}
+    >
+      {!selectedValue ? <p>{defaultValue}</p> : <p>{selectedValue}</p>}
+      {!isDropDownOpen ? <DropDownGray4Icon width={20} height={20} /> : <DropDownGray3Icon width={20} height={20} />}
+    </div>
+  )
+}
+
+function DropBoxOptionBox({ children }: { children: ReactNode }) {
+  return (
+    <div className="border-gray2 absolute top-17 max-h-[400px] w-full overflow-y-scroll rounded-[12px] border bg-white p-2 drop-shadow-md">
+      {children}
+    </div>
+  )
+}
+function DropBoxOptionItem({ children, onClick }: { children: ReactNode; onClick: () => void }) {
+  return (
+    <div onClick={onClick} className="hover:bg-gray1 kr-button text-gray4 w-full px-2 py-[18px]">
+      {children}
+    </div>
+  )
+}
+
+DropDown.DropBoxOptionBox = DropBoxOptionBox
+DropDown.DropBoxOptionItem = DropBoxOptionItem
