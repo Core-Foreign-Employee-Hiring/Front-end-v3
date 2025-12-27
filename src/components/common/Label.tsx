bfcabcb0ab29a4d3e3e06174aeb8129f92f0b934
유림 <yulimh001@gmail.com>
2025-12-27 14:07:00 +0900

    style: Label 기본 스타일 템플릿 부여

--- src/components/common/Label.tsx
+++ src/components/common/Label.tsx
@@ -1,15 +1,28 @@
 import { ReactNode } from 'react'
 
 interface LabelProps {
+  type?: 'h1' | 'h2' | 'h3' | 'inputLabel'
   className?: string
   isRequired?: boolean
   rightElement?: ReactNode
   label: string
 }
-export default function Label({ className = 'kr-title-md', rightElement, isRequired, label }: LabelProps) {
+const labelType = {
+  h1: 'kr-title-lg text-black',
+  h2: 'kr-title-md text-black',
+  h3: 'kr-subtitle-lg text-black',
+  inputLabel: 'kr-title-sm text-gray5',
+}
+export default function Label({
+  className = 'kr-title-md',
+  rightElement,
+  isRequired,
+  label,
+  type = 'inputLabel',
+}: LabelProps) {
   return (
-    <div className="flex justify-between">
-      <div className={`${className} flex gap-x-1`}>
+    <div className="flex items-center justify-between">
+      <div className={`${labelType[type]} ${className} flex gap-x-1`}>
         <h1>{label}</h1>
         {isRequired && <p className="text-main-500">*</p>}
       </div>
