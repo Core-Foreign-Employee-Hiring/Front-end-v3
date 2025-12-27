67cdba42d18b7c26b8f3871d3e66a0df0d16f28b
유림 <yulimh001@gmail.com>
2025-12-27 14:01:59 +0900

    feat: import index.ts

---
+++ src/components/Spec/index.ts
@@ -0,0 +1,49 @@
+export { default as SpecEducation } from './SpecEducation'
+export { default as SpecCareer } from './SpecCareer'
+export { default as SpecCertification } from './SpecCertification'
+export { default as SpecExperience } from './SpecExperience'
+export { default as SpecLanguage } from './SpecLanguage'
+export { default as ProgressBar } from './ProgressBar'
+export { default as BottomButton } from './BottomButton'
+
+//education
+export { default as AddEduForm } from './education/AddEduForm'
+export { default as EduDuration } from './education/EduDuration'
+export { default as EduGrades } from './education/EduGrades'
+export { default as EduMajor } from './education/EduMajor'
+export { default as EduSchool } from './education/EduSchool'
+
+//languageSkill
+export { default as AddLangForm } from './language/AddLangForm'
+
+//certification
+export { default as AddCertForm } from './certification/AddCertForm'
+export { default as CertName } from './certification/CertName'
+export { default as CertAcquiredDate } from './certification/CertAcquiredDate'
+export { default as CertDocumentURL } from './certification/CertDocumentURL'
+
+//carrer
+export { default as AddCareerForm } from './carrer/AddCareerForm'
+export { default as CareerCompanyName } from './carrer/CareerCompanyName'
+export { default as CareerPosition } from './carrer/CareerPosition'
+export { default as CareerDuration } from './carrer/CareerDuration'
+export { default as CareerContractType } from './carrer/CareerContractType'
+export { default as CareerHighlight } from './carrer/CareerHighlight'
+
+//activity
+export { default as AddActivityForm } from '@/components/spec/activity/AddActivityForm'
+export { default as AddExperienceForm } from '@/components/spec/activity/experience/AddExperienceForm'
+export { default as AddAwardForm } from '@/components/spec/activity/award/AddAwardForm'
+
+//award
+export { default as AwardAcquiredDate } from '@/components/spec/activity/award/AwardAcquiredDate'
+export { default as AwardDescription } from '@/components/spec/activity/award/AwardDescription'
+export { default as AwardDocumentUrl } from '@/components/spec/activity/award/AwardDocumentUrl'
+export { default as AwardHost } from '@/components/spec/activity/award/AwardHost'
+export { default as AwardName } from '@/components/spec/activity/award/AwardName'
+
+//experience
+export { default as Experience } from '@/components/spec/activity/experience/Experience'
+export { default as ExpImprovementRate } from '@/components/spec/activity/experience/ExpImprovementRate'
+export { default as ExpDuration } from '@/components/spec/activity/experience/ExpDuration'
+export { default as ExpDescription } from '@/components/spec/activity/experience/ExpDescription'
---
+++ src/components/common/index.ts
@@ -0,0 +1,8 @@
+export { default as Button } from './Button'
+export { default as Header } from './Header'
+export { default as Spacing } from './Spacing'
+export { default as Label } from './Label'
+export { default as PageLayout } from './PageLayout'
+export { default as TextInput } from './TextInput'
+export { default as DropDown } from './DropDown'
+export { default as UploadItem } from './UploadItem'
