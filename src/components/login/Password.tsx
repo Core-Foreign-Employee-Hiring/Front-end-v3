f80931575d4ca2dd8883e6f4897418106c3595eb
유림 <yulimh001@gmail.com>
2025-12-29 21:31:38 +0900

    feat: 로그인 기능 구현

--- src/components/login/Password.tsx
+++ src/components/login/Password.tsx
@@ -1,28 +1,29 @@
 'use client'
 
 import { useAuthStore } from '@/store/authStore'
-import { usePasswordVisible } from '@/hooks'
 
+import { usePasswordVisible } from '@/hooks'
 import { Label, Spacing, TextInput } from '@/components/common'
 
 export default function Password() {
-  const password = useAuthStore((state) => state.login.password)
-  const setLogin = useAuthStore((state) => state.setLogin)
+  const { login, error } = useAuthStore((state) => state)
 
-  const { toggleVisibility, VisibilityIcon, inputType } = usePasswordVisible()
+  const { handlePasswordChange, handleToggleClick, VisibilityIcon, inputType } = usePasswordVisible()
 
   return (
     <div className="w-full">
       <Label label="비밀번호" type="subtitleLg" />
       <Spacing height={8} />
       <TextInput
+        status={error ? 'error' : 'default'}
+        helperText={error ? error : undefined}
         inputType={inputType}
-        value={password ?? ''}
-        onChange={(e) => setLogin({ password: e.target.value })}
+        value={login.password ?? ''}
+        onChange={handlePasswordChange}
         placeholder="비밀번호를 입력해주세요."
         rightElement={
           <VisibilityIcon
-            onClick={toggleVisibility}
+            onClick={() => handleToggleClick}
             className="text-gray4 hover:text-main-500 cursor-pointer transition-colors"
             height={24}
             width={24}
