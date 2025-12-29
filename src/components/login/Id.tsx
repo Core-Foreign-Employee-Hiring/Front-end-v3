96c8e03ce6651dfa63ad088614c70555b9dcdbb2
유림 <yulimh001@gmail.com>
2025-12-29 21:35:15 +0900

    feat: Id 컴포넌트 기능 구현

--- src/components/login/Id.tsx
+++ src/components/login/Id.tsx
@@ -3,20 +3,24 @@
 import { useAuthStore } from '@/store/authStore'
 
 import { Label, Spacing, TextInput } from '@/components/common'
+import { ChangeEvent } from 'react'
 
 export default function Id() {
-  const userId = useAuthStore((state) => state.login.userId)
-  const setLogin = useAuthStore((state) => state.setLogin)
+  const { login, error, setLogin, resetAuthStatus } = useAuthStore((state) => state)
+
+  const handleIdChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setLogin({ userId: e.target.value })
+    if (error) resetAuthStatus()
+  }
 
   return (
     <div className="w-full">
       <Label type={'subtitleLg'} label={'아이디'} />
       <Spacing height={8} />
       <TextInput
-        onChange={(e) => {
-          setLogin({ userId: e.target.value })
-        }}
-        value={userId ?? ''}
+        status={error ? 'error' : 'default'}
+        onChange={handleIdChange}
+        value={login.userId ?? ''}
         placeholder="아아디를 입력해주세요."
       />
     </div>
