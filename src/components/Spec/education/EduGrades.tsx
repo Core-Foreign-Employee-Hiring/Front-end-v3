a62a78b04e2dec955d1218a0628ede41abd231a6
유림 <yulimh001@gmail.com>
2025-12-31 09:30:24 +0900

    feat: 학점, 총점 0~4.5 사이의 값으로 제한

--- src/components/Spec/education/EduGrades.tsx
+++ src/components/Spec/education/EduGrades.tsx
@@ -16,7 +16,7 @@ export default function EduGrades() {
   // 2. 숫자로 변환하여 업데이트하는 로직
   const updateField = (value: string, fieldName: 'earnedScore' | 'maxScore') => {
     // 입력값이 빈 문자열이면 0으로 처리하거나, 숫자 타입으로 변환
-    const numValue = value === '' ? 0 : Number(value)
+    const numValue = value === '' ? '' : Number(value)
 
     setEducation({
       ...education,
@@ -42,7 +42,22 @@ export default function EduGrades() {
   // score가 undefined나 null일 경우를 대비한 안전한 비교
   const getStatus = () => {
     const { earnedScore, maxScore } = education
+    if (Number(earnedScore) < 0 || Number(earnedScore) > 4.5) {
+      return 'error'
+    }
+
+    if (Number(maxScore) < 0 || Number(maxScore) > 4.5) {
+      return 'error'
+    }
+
     return Number(earnedScore) > Number(maxScore) ? 'error' : 'default'
+  }
+
+  const getHelperText = () => {
+    const { earnedScore, maxScore } = education
+    if (Number(earnedScore) < 0 || Number(earnedScore) > 4.5) return '학점은 0점 ~ 4.5점 사이 값이어야 합니다.'
+    if (Number(maxScore) < 0 || Number(maxScore) > 4.5) return '총점은 0점 ~ 4.5점 사이 값이어야 합니다.'
+    if (Number(earnedScore) > Number(maxScore)) return '학점이 총점보다 낮아야 합니다.'
   }
 
   return (
@@ -52,12 +67,12 @@ export default function EduGrades() {
       <div className="flex w-full gap-x-4">
         <TextInput
           status={getStatus()}
-          helperText={'학점이 총점보다 낮아야 합니다.'}
+          helperText={getHelperText()}
           onFocus={(e) => handleFocus(e, 'earnedScore')}
           onBlur={(e) => handleBlur(e, 'earnedScore')}
           onChange={(e) => updateField(e.target.value, 'earnedScore')}
           inputType={'number'}
-          value={education.earnedScore ?? 0} // nullish 대비
+          value={education.earnedScore ?? 0}
           placeholder={'학점'}
         />
         <TextInput
@@ -66,7 +81,7 @@ export default function EduGrades() {
           onBlur={(e) => handleBlur(e, 'maxScore')}
           onChange={(e) => updateField(e.target.value, 'maxScore')}
           inputType={'number'}
-          value={education.maxScore ?? 0} // nullish 대비
+          value={education.maxScore ?? 0}
           placeholder={'총점'}
         />
       </div>
