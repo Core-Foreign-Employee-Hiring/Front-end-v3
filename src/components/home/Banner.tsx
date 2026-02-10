73d8c4d82b0a272d5be7a0c7f55fe0bdb9e69082
유림 <yulimh001@gmail.com>
2026-02-10 19:32:55 +0900

    style: banner 반응형

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -9,10 +9,19 @@ export default function Banner() {
     <div className="relative h-[300px] w-full overflow-hidden">
       <div className="relative z-10 flex h-full flex-col items-center justify-center gap-y-[32px]">
         <div className="flex flex-col gap-y-2">
-          <h1 className="kr-resume-lg text-center text-white">막막했던 한국 취업·창업 준비 KORFIT과 함께</h1>
+          <div className="desktop:flex-row flex flex-col gap-x-1 gap-y-1">
+            <h1 className="kr-title-lg desktop:kr-resume-lg text-center text-white">막막했던 한국 취업·창업 준비 </h1>
+            <h1 className="kr-title-lg desktop:kr-resume-lg text-center text-white">KORFIT과 함께</h1>
+          </div>
+
           <div className="flex flex-col items-center justify-center">
-            <p className="kr-title-sm text-white">복잡한 채용 절차부터 취업 역량 강화까지</p>
-            <p className="kr-title-sm text-white">글로벌 인재를 위한 한국 커리어 준비의 모든 과정을 지원합니다</p>
+            <p className="kr-subtitle-md desktop:kr-title-sm desktop:block tablet:block hidden text-white">
+              복잡한 채용 절차부터 취업 역량 강화까지
+            </p>
+            <div className="desktop:flex-row flex flex-col items-center gap-x-1">
+              <p className="kr-subtitle-md desktop:kr-title-sm text-white">글로벌 인재를 위한 한국 커리어 준비의</p>
+              <p className="kr-subtitle-md desktop:kr-title-sm text-white">모든 과정을 지원합니다</p>
+            </div>
           </div>
         </div>
         <Button
