00dd8945ce826db762f21d5513b47dff3ccee470
유림 <yulimh001@gmail.com>
2026-02-11 19:39:56 +0900

    style: CTA Button 높이 고정

--- src/components/home/CTACard.tsx
+++ src/components/home/CTACard.tsx
@@ -17,10 +17,13 @@ export default function CTACard({ Icon, title, description, path }: CTACardProps
     router.push(path)
   }
   return (
-    <div className="desktop:p-5 tablet:p-5 flex w-full flex-col gap-y-3 rounded-[12px] bg-white p-4">
-      <div className="bg-main-300 flex h-[40px] w-[40px] items-center justify-center rounded-[6px]">{Icon}</div>
-      <p className="kr-subtitle-lg">{title}</p>
-      <p className="body-sm text-gray5">{description}</p>
+    <div className="desktop:p-5 tablet:p-5 flex w-full flex-col justify-between gap-y-3 rounded-[12px] bg-white p-4">
+      <div className="flex flex-col gap-y-3">
+        <div className="bg-main-300 flex h-[40px] w-[40px] items-center justify-center rounded-[6px]">{Icon}</div>
+        <p className="kr-subtitle-lg">{title}</p>
+        <p className="body-sm text-gray5">{description}</p>
+      </div>
+
       <Button onClick={onNavigate} variant={'secondary'} size={'md'}>
         시작하기
       </Button>
