eff098de12f5603135e1830cace4fdac8d56774e
유림 <yulimh001@gmail.com>
2026-02-26 20:08:36 +0900

    feat: 노트 삭제 toast 추가

--- src/components/common/modal/DeleteNoteModal.tsx
+++ src/components/common/modal/DeleteNoteModal.tsx
@@ -5,9 +5,11 @@ import { deleteNote } from '@/lib/client/interview'
 import { useModalStore } from '@/store/modalStore'
 import { useNoteStore } from '@/store/interview/noteStore'
 import { useTranslation } from 'react-i18next'
+import { useToast } from '@/components/common/toast/ToastContext'
 
 export default function DeleteNoteModal() {
-  const { t } = useTranslation('modal')
+  const { t } = useTranslation(['modal', 'message'])
+  const { success, error } = useToast()
   const { toggleModal, modals } = useModalStore((state) => state)
 
   const { selectedNoteId } = useNoteStore((state) => state)
@@ -24,26 +26,30 @@ export default function DeleteNoteModal() {
       isOpen={modals.isDeleteNoteModalOpen}
     >
       <Modal.Header>
-        <Label label={t('delete_note.header')} type={'subtitleLg'} />
+        <Label label={t('modal:delete_note.header')} type={'subtitleLg'} />
       </Modal.Header>
       <Modal.Body>
-        <Label label={t('delete_note.body')} type={'subtitleMd'} labelColor={'text-gray5'} />
+        <Label label={t('modal:delete_note.body')} type={'subtitleMd'} labelColor={'text-gray5'} />
       </Modal.Body>
       <Modal.Footer>
         <>
           <Button onClick={toggleDeleteNoteState} variant={'outline'} size={'lg'} buttonType={'button'}>
-            {t('footer_buttons.cancel')}
+            {t('modal:footer_buttons.cancel')}
           </Button>
           <Button
             onClick={async () => {
               const result = await deleteNote(selectedNoteId)
-              console.log('노트 삭제', result.data)
+              if (result.success) {
+                success(t('message:delete_note.success.title'), t('message:delete_note.success.description'))
+              } else {
+                error(t('message:delete_note.error.title'), t('message:delete_note.error.description'))
+              }
             }}
             variant={'primary'}
             size={'lg'}
             buttonType={'button'}
           >
-            {t('footer_buttons.save')}
+            {t('modal:footer_buttons.save')}
           </Button>
         </>
       </Modal.Footer>
