670bcac2b1f2303cb69a1c2ae511ca97f7cedaf8
유림 <yulimh001@gmail.com>
2026-04-20 10:56:48 +0900

    feat: promotion 페이지 제작

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -2,38 +2,23 @@
 
 import Image from 'next/image'
 import { useTranslation } from 'react-i18next'
+import { useRouter } from 'next/navigation'
 
 export default function Banner() {
   const { t } = useTranslation(['home'])
+  const router = useRouter()
   return (
-    <div className="relative h-[300px] w-full overflow-hidden px-[20px]">
+    <div
+      onClick={() => {
+        router.push('/promotion')
+      }}
+      className="desktop:h-[400px] tablet:h-[280px] relative h-[200px] w-full cursor-pointer overflow-hidden px-[20px]"
+    >
       <div className="relative z-10 flex h-full flex-col items-center justify-center gap-y-[32px]">
-        <div className="flex flex-col gap-y-2">
-          <h1 className="kr-title-lg desktop:kr-resume-lg text-center text-white">{t('banner.h1')} </h1>
-
-          <div className="flex flex-col items-center justify-center">
-            <p className="kr-subtitle-md desktop:kr-title-sm text-center text-white">{t('banner.description')}</p>
-          </div>
-        </div>
-        {/*<Button*/}
-        {/*  rightIcon={<ArrowFowardMain500Icon width={20} height={20} />}*/}
-        {/*  onClick={() => {}}*/}
-        {/*  variant={'secondary'}*/}
-        {/*  size={'lg'}*/}
-        {/*  customClassName={'w-[313px]'}*/}
-        {/*>*/}
-        {/*  {t('banner.button')}*/}
-        {/*</Button>*/}
+        {/* 주석 처리된 콘텐츠 유지 */}
       </div>
 
-      {/* 3. 배경 이미지 (z-index를 따로 주지 않으면 기본적으로 z-0입니다) */}
-      <Image
-        src={'/banner-img.svg'}
-        alt={'배너'}
-        className={'object-cover'}
-        fill
-        priority // 배너 이미지는 우선순위 로딩을 권장합니다.
-      />
+      <Image src={'/riza-banner-img.png'} alt={'배너'} className={'object-cover'} fill priority />
     </div>
   )
 }
